9a2d1238998e014e2f7e4630b3b377bb87eb3b93
Ruben Koster <rkoster@starkandwayne.com>
2018-06-05 13:48:02 +0200

    Added missing BBL_ENV_NAME env var

--- template/down.sh
+++ template/down.sh
@@ -2,6 +2,7 @@
 
 pushd $( dirname "${BASH_SOURCE[0]}" )
 
+export BBL_ENV_NAME=$(basename $(pwd))
 export BBL_IAAS=gcp
 export BBL_GCP_REGION=us-central1
 export BBL_GCP_SERVICE_ACCOUNT_KEY=${BBL_ENV_NAME}_gcp_credentials.json
