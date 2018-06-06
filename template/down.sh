720e4b5f141af7e372d072dcbc708eececbac7a8
Ruben Koster <rkoster@starkandwayne.com>
2018-06-06 13:31:23 +0200

    remove-iam-policy-binding on down

--- template/down.sh
+++ template/down.sh
@@ -5,5 +5,10 @@ source ./common.sh
 
 bbl down --no-confirm
 
+gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
+       --member serviceAccount:${SERVICE_ACCOUNT} \
+       --role "roles/editor"
+
 gcloud --quiet iam service-accounts delete ${SERVICE_ACCOUNT}
+
 rm -f ${BBL_GCP_SERVICE_ACCOUNT_KEY}
