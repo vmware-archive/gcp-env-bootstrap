84a92a96286acb1051459088d1ff4342ae801a6d
Ruben Koster <rkoster@starkandwayne.com>
2018-06-07 13:00:28 +0200

    Added netcat for `bbl ssh`

--- install_deps.sh
+++ install_deps.sh
@@ -5,4 +5,4 @@ echo "deb http://apt.starkandwayne.com stable main" | sudo tee /etc/apt/sources.
 sudo apt update
 sudo apt install -y bosh-bootloader bosh-cli
 sudo apt install -y libreadline6 libreadline6-dev
-sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 make tree
+sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 make tree netcat-openbsd
