6a7ed8f19b347559cd1d6362fd40d66396badb0a
Ruben Koster <rkoster@starkandwayne.com>
2018-06-13 14:28:42 +0200

    Remove port and set username for ssh

--- template/up.sh
+++ template/up.sh
@@ -27,13 +27,13 @@ EOF
 chmod 600 \${JUMPBOX_PRIVATE_KEY}
 export JUMPBOX_SSH_CONFIG=\$(mktemp)
 cat > \${JUMPBOX_SSH_CONFIG} <<-EOF
-ProxyCommand ssh -W %h:%p $(bosh int vars/jumpbox-vars-file.yml --path /external_ip) -p 22 -i \${JUMPBOX_PRIVATE_KEY}
+ProxyCommand ssh -W %h:%p jumpbox@$(bosh int vars/jumpbox-vars-file.yml --path /external_ip) -p 22 -i \${JUMPBOX_PRIVATE_KEY}
 EOF
 export DIRECTOR_PRIVATE_KEY=\$(mktemp)
 cat > \${DIRECTOR_PRIVATE_KEY} <<-EOF
 $(bosh int vars/director-vars-store.yml --path /jumpbox_ssh/private_key)
 EOF
 chmod 600 \${DIRECTOR_PRIVATE_KEY}
-export DIRECTOR_SSH=jumpbox@$(bosh int vars/director-vars-file.yml --path /internal_ip):22
+export DIRECTOR_SSH=jumpbox@$(bosh int vars/director-vars-file.yml --path /internal_ip)
 $(bbl print-env | sed -e 's@=/.*@=${JUMPBOX_PRIVATE_KEY}@g')
 OEOF
