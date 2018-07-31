7a0ffa019024fdc71c323cce1e2b239d92386669
Alan McGinlay <amcginlay@users.noreply.github.com>
2018-07-31 16:31:51 +0100

    #159429575
    script failing as libreadline dependencies folded up

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,7 @@ wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/
 echo "deb http://apt.starkandwayne.com stable main" | sudo tee /etc/apt/sources.list.d/starkandwayne.list
 sudo apt update
 sudo apt install -y bosh-bootloader bosh-cli
-sudo apt install -y libreadline6 libreadline6-dev
+sudo apt install -y libreadline
 sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 make tree netcat-openbsd
 
 mkdir -p ~/.bosh
