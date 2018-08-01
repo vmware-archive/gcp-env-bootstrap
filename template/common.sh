a8ddfc6c1d29d5bad8c678354d87a2f56c1210af
Alan McGinlay <amcginlay@users.noreply.github.com>
2018-08-01 09:55:08 +0100

    Update common.sh

--- template/common.sh
+++ template/common.sh
@@ -6,3 +6,8 @@ export BBL_IAAS=gcp
 export BBL_GCP_REGION=us-central1
 export BBL_GCP_SERVICE_ACCOUNT_KEY=${BBL_ENV_NAME}-service-account-key.json
 export SERVICE_ACCOUNT=${BBL_ENV_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+
+if ! [ -x "$(command -v bbl)" ]; then
+    echo "Installing dependencies"
+    ./install_deps.sh
+fi
