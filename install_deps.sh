b255e05711a8b0a9b44469ddd0d8e2eb650cb6f7
Alan McGinlay <amcginlay@users.noreply.github.com>
2018-08-15 13:47:02 -0600

    Remove GShell specific "hack"

--- install_deps.sh
+++ install_deps.sh
@@ -6,7 +6,3 @@ sudo apt update
 sudo apt install -y bosh-bootloader bosh-cli
 sudo apt install -y libreadline
 sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 make tree netcat-openbsd
-
-mkdir -p ~/.bosh
-rm -fr ~/.bosh/installations
-ln -s /tmp ~/.bosh/installations
