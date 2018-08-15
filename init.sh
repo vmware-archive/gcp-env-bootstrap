62a75a2ede0ae5b7081a1a6925a76277c103e400
Alan McGinlay <amcginlay@users.noreply.github.com>
2018-08-15 14:47:18 -0600

    Script takes args for envs

--- init.sh
+++ init.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-while true; do
-    read -p "How many student environments do you want to create: " total
-    if ! [[ "${total}" =~ ^[0-9]+$ ]]
-    then
-        echo "Please enter a number"; exit
-    fi
-    break
-done
+SCRIPTDIR=$(cd $(dirname "$0") && pwd -P)
 
-while true; do
-    read -p "Do you wish to create: ${total} student environments [yes/no]: " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
+if [ $# -eq 0 ]; then
+  echo "no args provided"
+  exit 1
+fi
+
+echo "The following environments will be created:"
+for env in "$@"
+do
+    echo "- $env"
 done
+read -p "Are you sure? " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 2
+fi
 
 if ! [ -x "$(command -v bbl)" ]; then
     echo "Installing dependencies"
-    ./install_deps.sh
+    ${SCRIPTDIR}/install_deps.sh
 fi
 
-start=1
-for ((i=start; i<=${total}; i++))
+for env in "$@"
 do
-    dir="student-env-${i}"
-    mkdir -p ${dir}; pushd ${dir} > /dev/null
-    find ../template -mindepth 1 -maxdepth 1 -type f -exec ln -sf {} \;
-    for dir in ops terraform; do
-        mkdir -p ${dir}; pushd ${dir} > /dev/null
-        find ../../template/${dir} -mindepth 1 -maxdepth 1 -type f -exec ln -sf {} \;
-        popd > /dev/null
-    done
-    popd > /dev/null
-    echo "Created student-env-${i}"
+  dir="${SCRIPTDIR}/envs/${env}"
+  mkdir -p ${dir}; pushd ${dir} > /dev/null
+  find ${SCRIPTDIR}/template -mindepth 1 -maxdepth 1 -type f -exec ln -sf {} \;
+  for dir in ops terraform; do
+      mkdir -p ${dir}; pushd ${dir} > /dev/null
+      find ${SCRIPTDIR}/template/${dir} -mindepth 1 -maxdepth 1 -type f -exec ln -sf {} \;
+      popd > /dev/null
+  done
+  popd > /dev/null
+  echo "Created ${env}"
 done
+
 echo "Student environments created"
