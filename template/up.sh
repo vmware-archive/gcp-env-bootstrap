4fd8ffd559a228fc308c02d3ce95793c44af2c68
Chad Peterson <cpeterson@pivotal.io>
2019-09-06 09:52:27 -0600

    adding this file to have instructors as part of projects as owners

--- template/up.sh
+++ template/up.sh
@@ -36,6 +36,10 @@ if [ ! -f "${BBL_GCP_SERVICE_ACCOUNT_KEY}" ]; then
     --member user:$(cat ../admins.txt) \
     --role "roles/owner"
 
+  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member user:$(cat ../instructors.txt) \
+    --role "roles/owner"
+
 fi
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
